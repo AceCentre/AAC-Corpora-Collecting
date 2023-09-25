0a54fbe17bd62d5c64601774de14d63121db28c8
will wade <willwade@gmail.com>
2023-09-25 11:09:07 +0100

    Update Grid-OBF-WIP.py

--- Grid-OBF-WIP.py
+++ Grid-OBF-WIP.py
@@ -73,3 +73,83 @@ with open(obf_file_path, 'w', encoding='utf-8') as f:
     f.write(obf_json)
 
 obf_file_path
+
+
+// What follows is a second type for more complex 
+
+# Importing necessary libraries
+import xml.etree.ElementTree as ET
+import json
+
+def grid_to_openboard(grid_xml_path, styles_xml_path, output_json_path):
+    """
+    Convert a Grid 3 board to OpenBoard format.
+
+    Parameters:
+    - grid_xml_path: str, path to Grid 3 board XML file
+    - styles_xml_path: str, path to Grid 3 styles XML file
+    - output_json_path: str, path to output OpenBoard JSON file
+    """
+    
+    # Parse Grid XML file
+    tree = ET.parse(grid_xml_path)
+    root = tree.getroot()
+    
+    # Parse styles XML file
+    style_tree = ET.parse(styles_xml_path)
+    style_root = style_tree.getroot()
+    
+    # Extract Grid 3 styles and map them to OpenBoard button styles
+    style_mapping = {}
+    for style in style_root.findall(".//Style"):
+        key = style.attrib.get('Key')
+        back_colour = style.find('BackColour').text if style.find('BackColour') is not None else '#FFFFFFFF'
+        font_colour = style.find('FontColour').text if style.find('FontColour') is not None else '#000000FF'
+        style_mapping[key] = {
+            'backgroundColor': back_colour[:7],  # Remove alpha channel
+            'textColor': font_colour[:7]  # Remove alpha channel
+        }
+    
+    # Create OpenBoard JSON structure
+    openboard_json = {
+        'id': 'board',
+        'name': 'Board',
+        'width': len(root.find('.//ColumnDefinitions').findall('ColumnDefinition')),
+        'height': len(root.find('.//RowDefinitions').findall('RowDefinition')),
+        'buttons': []
+    }
+    
+    # Convert Grid 3 buttons to OpenBoard buttons
+    for button in root.findall(".//Button"):
+        style_key = button.find('StyleKey').text if button.find('StyleKey') is not None else 'Default'
+        style = style_mapping.get(style_key, {})
+        
+        # Extract label from Caption or WordList
+        label = ''
+        caption = button.find('Caption')
+        if caption is not None:
+            label = caption.text
+        else:
+            wordlist = button.find('WordList')
+            if wordlist is not None:
+                label = wordlist.find('.//Item').find('Name').text  # Use the first word list item as label
+
+        openboard_button = {
+            'id': button.attrib.get('Id'),
+            'label': label,
+            'row': int(button.find('GridRow').text),
+            'col': int(button.find('GridColumn').text),
+            'width': int(button.find('GridWidth').text),
+            'height': int(button.find('GridHeight').text),
+            'style': style
+        }
+        openboard_json['buttons'].append(openboard_button)
+    
+    # Save OpenBoard JSON file
+    with open(output_json_path, 'w') as f:
+        json.dump(openboard_json, f, indent=4)
+
+# Example usage
+grid_to_openboard('/path/to/grid.xml', '/path/to/styles.xml', '/path/to/output.json')
+
+
