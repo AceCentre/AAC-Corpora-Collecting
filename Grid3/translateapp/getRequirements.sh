3b5edc289e52061eca1f9695a4418d4b568a0d22
will wade <willwade@gmail.com>
2024-12-09 13:20:16 +0000

    fix requirements

--- Grid3/translateapp/getRequirements.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-echo "Updating requirements.txt"
-uv sync
-uv pip compile pyproject.toml -o requirements.txt >/dev/null
-echo "-e ." >>requirements.txt
-echo "Successfully updated requirements.txt"
